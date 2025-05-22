aaf6bb59b3736ee0efe64868e99137cbd418017f
Eglis-dogjani <eglidogjani@gmail.com>
2025-05-22 12:34:07 -0700

    Language JS

---
+++ scripts/lang.js
@@ -0,0 +1,69 @@
+var arrLang = {
+    'en': {
+        'home': 'Home',        
+        'about': 'About us',
+        'destinations':'Destinations',
+        'contact':'Contact',
+        'quote':'Become a traveller! Do you want to explore this beautiful world???',
+        'title':'Welcome to Aventurati – Your Gateway to Unforgettable Journeys!',
+        'titledesc':'For first-time travelers in Albania, Aventurati Website is your ultimate guide to unforgettable adventure and outdoor escapes. From rugged mountain treks to hidden waterfall hikes, we connect thrill-seekers and nature lovers with curated travel experiences, expert tips, and real stories from explorers like you. Step off the beaten path and into your next great journey',
+        'r1':'Wonderful Views',
+        'r2':'Smiling faces everywhere',        
+        'r3':'Budget-friendly',
+        'rd1':'Whether you’re visiting the wild north or the mysterious south, Albania is filled with enough scenery to put your wanderlust into overdrive. From the stunning national parks of Thethi and Llogara to the the glistening turquoise waters of the Adriatic, Albania provides a permanent wow factor for those looking for a jaw-dropping view.',
+        'rd2':'Albania is also known for its hospitality. The people in Albania are warm and welcoming. You can easily find smiling faces in tourist spots, neighborhoods, restaurants, and bars. They would greet you with a smile and are likely to welcome you to their house. Moreover, a lot of people in Albania also speak English. You will find it very comfortable to visit Albania and explore the country.',
+        'rd3':'Many tourists confirm that the prices are cheap when it comes to the amazing experience if offers. You can travel here for quite a bit less than you would pay in the likes of Greece and Italy, making it a great alternative to these Mediterranean hotspots.',        
+        'dest1':'Explore Top Destinations',
+        'destc1':'Here is the recommended places in Albania and my recommendations for each spot for your visit to the cities of the country.',        
+        'cnt1':'Contact For Any Query',
+        'cntbtn':'Send Message',
+    },
+    'al': {
+        'home': 'Shtepia',        
+        'about': 'Rreth nesh',
+        'destinations':'Destinacione',
+        'contact':'Kontakti',
+        'quote':'Bëhu udhëtar! A dëshiron të eksplorosh këtë botë të bukur???',        
+        'title':'Mirë se vini në Aventurati – Porta Juaj për Udhëtime të Paharrueshme!',
+        'titledesc':'Për udhëtarët që udhëtojnë për herë të parë në Shqipëri, faqja e internetit Aventurati është udhëzuesi juaj përfundimtar për aventura të paharrueshme dhe arratisje në natyrë. Nga shtegjet e ashpra malore deri te shëtitjet në ujëvara të fshehura, ne i lidhim kërkuesit e emocioneve dhe dashamirësit e natyrës me përvoja udhëtimi të kuruara, këshilla nga ekspertët dhe histori të vërteta nga eksplorues si ju. Dilni nga shtigjet e rrahura dhe filloni udhëtimin tuaj të ardhshëm të mrekullueshëm.',
+        'r1':'Pamje të mrekullueshme',
+        'r2':'Njerëz të mrekullueshëm',        
+        'r3':'Buxhet i ulët',        
+        'rd1':'Pavarësisht nëse po vizitoni veriun e egër apo jugun misterioz, Shqipëria është plot me peizazhe të mjaftueshme për ta çuar në pah dëshirën tuaj për të udhëtuar. Nga parqet kombëtare mahnitëse të Thethit dhe Llogorasë deri te ujërat e shndritshme bruze të Adriatikut, Shqipëria ofron një faktor të përhershëm mahnitës për ata që kërkojnë një pamje mahnitëse.',
+        'rd2':'Shqipëria njihet gjithashtu për mikpritjen e saj. Njerëzit në Shqipëri janë të ngrohtë dhe mikpritës. Mund të gjeni lehtësisht fytyra të qeshura në vendet turistike, lagjet, restorantet dhe baret. Ata do tju përshëndesin me një buzëqeshje dhe ka të ngjarë tju mirëpresin në shtëpinë e tyre. Për më tepër, shumë njerëz në Shqipëri flasin edhe anglisht. Do ta gjeni shumë të rehatshme të vizitoni Shqipërinë dhe të eksploroni vendin.',
+        'rd3':'Shumë turistë konfirmojnë se çmimet janë të lira kur bëhet fjalë për përvojën e mrekullueshme që ofrohet. Mund të udhëtoni këtu për shumë më pak sesa do të paguanit në vende si Greqia dhe Italia, duke e bërë atë një alternativë të shkëlqyer ndaj këtyre vendeve të nxehta mesdhetare.',   
+        'dest1':'Eksploroni Top Destinacionet',
+        'destc1':'Ja vendet e rekomanduara në Shqipëri dhe rekomandimet e mia për secilin vend për vizitën tuaj në qytetet e vendit.',        
+        'cnt1':'Kontaktoni për çdo pyetje',
+        'cntbtn':'Dërgo Mesazh',  
+    },
+    'it': {
+        'home': 'Casa',        
+        'about': 'Chi siamo',
+        'destinations':'Destinazioni',
+        'contact':'Contatto',
+        'quote':'Diventa un viaggiatore! Vuoi esplorare questo meraviglioso mondo???',                
+        'title':'Benvenuti ad Aventurati: la vostra porta verso viaggi indimenticabili!',
+        'titledesc':'Per chi visita l Albania per la prima volta, il sito web Aventurati è la guida definitiva per avventure indimenticabili e fughe all aria aperta. Dalle escursioni in montagna più impervie alle escursioni alle cascate nascoste, mettiamo in contatto amanti del brivido e della natura con esperienze di viaggio selezionate, consigli di esperti e storie vere di esploratori come te. Esci dai sentieri battuti e parti per il tuo prossimo fantastico viaggio.',
+        'r1':'Viste meravigliose',
+        'r2':'Volti sorridenti ovunque',        
+        'r3':'Economico',
+        'rd1':'Che stiate visitando il selvaggio nord o il misterioso sud, l Albania è ricca di scenari in grado di far esplodere la vostra voglia di viaggiare. Dagli splendidi parchi nazionali di Thethi e Llogara alle scintillanti acque turchesi dell Adriatico, l Albania offre un fattore wow permanente a chi cerca un panorama mozzafiato.',
+        'rd2':'LAlbania è nota anche per la sua ospitalità. La gente del posto è calorosa e accogliente. È facile trovare volti sorridenti nei luoghi turistici, nei quartieri, nei ristoranti e nei bar. Vi accoglieranno con un sorriso e probabilmente vi daranno il benvenuto a casa loro. Inoltre, molti albanesi parlano anche inglese. Visitare lAlbania ed esplorare il paese sarà molto piacevole.',
+        'rd3':'Molti turisti confermano che i prezzi sono convenienti per l esperienza straordinaria che offre. Si può viaggiare qui spendendo molto meno di quanto si pagherebbe in paesi come Grecia e Italia, il che la rende un ottima alternativa a queste mete turistiche del Mediterraneo.',   
+        'dest1':'Esplora le migliori destinazioni',
+        'destc1':'Ecco i luoghi consigliati in Albania e i miei consigli per ogni tappa della vostra visita alle città del paese.',        
+        'cnt1':'Contattaci per qualsiasi domanda',
+        'cntbtn':'Invia messaggio',  
+    }
+};
+  // Process translation
+  $(function() {
+    $('.translate').click(function() {
+      var lang = $(this).attr('id');
+
+      $('.lang').each(function(index, item) {
+        $(this).text(arrLang[lang][$(this).attr('key')]);
+      });
+    });
+  });
